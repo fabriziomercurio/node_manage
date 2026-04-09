d8e9ac219adbc79c49c210bf994e986c1b0da1d5
fabriziomercurio <mercurio.fabrizio@gmail.com>
2026-04-09 12:04:35 +0200

    test:base routing with http module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import http from "node:http"; 
 
 const server = http.createServer((req,res) => {
+    if(req.method === 'GET') 
+    {
+       console.log("Metodo:", req.method);
+       console.log("Url:", req.url);
+       res.write(req.method);
+    }
     res.write("server is running!");
-    res.end();
+    res.end(); 
+    
 }) 
-
 server.listen(3000);
+    
